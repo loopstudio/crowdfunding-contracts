576dd0c17ca4c556ab44e0898524905a71ab2ccc
Marcos Martínez <mmartinezciompi@gmail.com>
2022-10-23 19:45:19 -0300

    Remove console from contract

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "hardhat/console.sol";
 
 /// @title Crowdfunding
 /// @author @loopstudio
