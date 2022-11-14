3d556f1c3d26c472f7ce8cb76bed238da0667df4
FacuEM <facundomartinezesc@gmail.com>
2022-11-14 16:34:10 -0300

    Set goal as a param

--- tasks/launch.ts
+++ tasks/launch.ts
@@ -1,20 +1,23 @@
 import { task } from "hardhat/config";
+import { utils } from "ethers";
 import moment from "moment";
 
 task("launch", "Launch a campaign")
   .addParam("address", "ERC20 token address")
+  .addParam("goal", "Amount of tokens needed to meet the campaign's goal.")
   .setAction(async (taskArgs, hre) => {
-    const { address } = taskArgs;
+    const { address, goal } = taskArgs;
     const start = moment().add(1, "day");
     const end = moment().add(21, "day");
 
     const Crowdfunding = await hre.ethers.getContractFactory("Crowdfunding");
-    const crowdfunding = await Crowdfunding.deploy(
-      address,
-      end.unix() - start.unix()
-    );
+    const crowdfunding = Crowdfunding.attach(address);
 
-    const launch = await crowdfunding.launch("100", start.unix(), end.unix());
+    const launch = await crowdfunding.launch(
+      utils.parseEther(goal),
+      start.unix(),
+      end.unix()
+    );
 
     if (launch) {
       console.log("Campaign launched");
