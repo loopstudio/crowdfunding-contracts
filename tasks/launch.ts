1e113119843516ac8cc5f5cc2d9f241ad8298121
FacuEM <facundomartinezesc@gmail.com>
2022-11-15 09:18:59 -0300

    Add start and end date as params

--- tasks/launch.ts
+++ tasks/launch.ts
@@ -1,22 +1,21 @@
 import { task } from "hardhat/config";
 import { utils } from "ethers";
-import moment from "moment";
 
 task("launch", "Launch a campaign")
-  .addParam("address", "ERC20 token address")
+  .addParam("address", "ERC20 token address.")
   .addParam("goal", "Amount of tokens needed to meet the campaign's goal.")
+  .addParam("start", "Campaign start date.")
+  .addParam("end", "Campaign end date.")
   .setAction(async (taskArgs, hre) => {
-    const { address, goal } = taskArgs;
-    const start = moment().add(1, "day");
-    const end = moment().add(21, "day");
+    const { address, goal, start, end } = taskArgs;
 
     const Crowdfunding = await hre.ethers.getContractFactory("Crowdfunding");
     const crowdfunding = Crowdfunding.attach(address);
 
     const launch = await crowdfunding.launch(
       utils.parseEther(goal),
-      start.unix(),
-      end.unix()
+      start,
+      end
     );
 
     if (launch) {
