f54c5ed803e8d2d84c40c1a3ea67473a167de175
Renato Cargnelutti <renato.cargnelutti8@gmail.com>
2022-11-21 19:24:55 -0300

    CROW-92 Delete unnecessary comment

--- tasks/allowance.ts
+++ tasks/allowance.ts
@@ -1,7 +1,6 @@
 import { utils } from "ethers";
 import { task } from "hardhat/config";
 
-// TODO: Update this!
 // example: npx hardhat allowance --amount 100 --tokenaddress 0x5FbDB2315678afecb367f032d93F642f64180aa3 --account 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512 --network localhost
 
 task("allowance", "Allowance for a given account")
